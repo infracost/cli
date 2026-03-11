08c5a2b1dfe9a5f0ec6612001372386eb9244a9d
Liam Cervante <liam.cervante@infracost.io>
2026-03-11 15:35:37 +0100

    fix: events must be lazily initialized

--- internal/api/events/config.go
+++ internal/api/events/config.go
@@ -11,19 +11,24 @@ var (
 type Config struct {
 	Endpoint string `env:"INFRACOST_CLI_EVENTS_ENDPOINT" flag:"events-endpoint;hidden" usage:"The endpoint for the Infracost events service" default:"https://pricing.api.infracost.io"`
 
-	Client func(httpClient *http.Client) Client
+	ClientFn func(httpClient *http.Client) Client
 }
 
-func (c *Config) Process() {
-	c.Client = func(httpClient *http.Client) Client {
-		// The events client may be used before config defaults are applied (e.g.
-		// to report early errors), so ensure the endpoint is always set.
-		if c.Endpoint == "" {
-			c.Endpoint = "https://pricing.api.infracost.io"
-		}
-		return &client{
-			client: httpClient,
-			config: c,
+func (c *Config) Client(httpClient *http.Client) Client {
+	if c.ClientFn == nil {
+		// The events client is kind of special in that it can be called anywhere in the CLI process (including outside
+		// the context of commands). This means that it may not have been initialized, so we have this lazy evaluation /
+		// initialization.
+		c.ClientFn = func(httpClient *http.Client) Client {
+			if c.Endpoint == "" {
+				// if we're here outside the context of commands, then this may also have never been set.
+				c.Endpoint = "https://pricing.api.infracost.io"
+			}
+			return &client{
+				client: httpClient,
+				config: c,
+			}
 		}
 	}
+	return c.ClientFn(httpClient)
 }
